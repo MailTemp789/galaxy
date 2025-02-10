081d0670ed89b78701d932692dd302d6278a1a25
kishanhadgala <mailtemp789@gmail.com>
2025-02-10 16:01:18 +0530

    from gi01

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload 10022025 - 2:36pm - 03:02pm
         </p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload from gi01
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
