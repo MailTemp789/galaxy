fb44e77bb9199faf70207eedc54caa0c51425e1c
kishan <mailtemp789@gmail.com>
2025-02-11 10:18:58 +0530

    banana

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload from gi12
         </p>
         <p>
-          Edit <code>src/App.js</code> apple
+          Edit <code>src/App.js</code> banana
         </p>
         <a
           className="App-link"
