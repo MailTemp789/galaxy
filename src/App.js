16e3d961c47bb067a8c8a552d98d48af93e07093
kishan <mailtemp789@gmail.com>
2025-02-11 11:30:30 +0530

    new 11022025

--- src/App.js
+++ src/App.js
@@ -13,11 +13,8 @@ function App() {
           Edit <code>src/App.js</code> and save to reload from gi12
         </p>
         <p>
-<<<<<<< HEAD
           Edit <code>src/App.js</code> i am sujal
-=======
           Edit <code>src/App.js</code> three new branch created
->>>>>>> kishan
         </p>
         <a
           className="App-link"
